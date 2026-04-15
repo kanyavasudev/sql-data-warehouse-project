dba2bcdcb1d397214ac188dfe5f6896f7337cb27
Kanya <74917282+kanyavasudev@users.noreply.github.com>
2026-04-15 09:36:07 +0530

    Enhance ddl_bronze.sql with comments and purpose
    Added comments and purpose description for the DDL script.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,8 +1,13 @@
--- =====================================================
--- BRONZE LAYER DATA LOADING SCRIPT
--- This script truncates existing tables
--- and loads raw CSV data into Bronze layer tables
--- =====================================================
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 SELECT '======================================';
 SELECT 'LOADING BRONZE LAYER';
