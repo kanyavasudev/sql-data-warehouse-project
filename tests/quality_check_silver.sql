6335ef141394540abd4948796b725715a04dee39
Kanya <74917282+kanyavasudev@users.noreply.github.com>
2026-04-15 20:15:49 +0530

    Enhance quality_check_silver.sql with comments
    Added detailed comments and usage notes for data quality checks.

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -1,8 +1,21 @@
--- =====================================================
--- Data Quality Checks for Silver Layer Tables
--- Purpose: Validate data before / after loading into Silver
--- =====================================================
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
 
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
 
 -- Check for nulls or duplicates in customer primary key
 -- Expectation: No results
